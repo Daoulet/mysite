91d200eac957363eebe8105ac4ef538146a9ed2f
Daulet Bimaganbetov <bimaganbetov1995@gmail.com>
2020-05-02 04:49:43 +0600

    post individ page

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,11 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 class HomePageView(ListView):
 	model = Post
 	template_name='home.html'
+
+class PostDetailView(DetailView):
+	model = Post
+	context_object_name = 'post'
+	template_name='post_detail_view.html'
