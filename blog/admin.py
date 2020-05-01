a75b01046020038ccf228bf815673d9a3ebd5e79
Daulet Bimaganbetov <bimaganbetov1995@gmail.com>
2020-05-02 04:30:54 +0600

    model Post listview

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Post
 
+class AdminSite(admin.ModelAdmin):
+	list_display = ("title", "author", "published_date")
+
+admin.site.register(Post, AdminSite)
 # Register your models here.
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Post(models.Model):
+	author = models.CharField(max_length=200)
+	title = models.CharField(max_length=200)
+	text = models.TextField(blank=True)
+	created_date = models.DateTimeField(default=timezone.now)
+	published_date = models.DateTimeField(blank=True, null=True)
+
+
+
+	def __str__(self):
+		return self.title
