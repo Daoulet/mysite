d1e5eece44de3f15cee989b16d0a8d49f128db54
Daulet Bimaganbetov <bimaganbetov1995@gmail.com>
2020-05-02 22:21:00 +0600

    search

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import HomePageView, PostDetailView
+from .views import HomePageView, PostDetailView, SearchResultsView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('<int:pk>', PostDetailView.as_view(), name='post_detail'),
+    path('search/', SearchResultsView.as_view(), name='search_results'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import ListView, DetailView
 from .models import Post
+from django.db.models import Q
 
 class HomePageView(ListView):
 	model = Post
@@ -9,3 +10,15 @@ class PostDetailView(DetailView):
 	model = Post
 	context_object_name = 'post'
 	template_name='post_detail_view.html'
+
+class SearchResultsView(ListView):
+	model = Post
+	context_object_name = 'post'
+	template_name='search_results.html'
+	
+	def get_queryset(self):
+		query = self.request.GET.get('q')
+		return Post.objects.filter(
+			Q(title__icontains=query) | Q (title__icontains=query)
+		)
+	
