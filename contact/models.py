6e1f0cf2fd1239164815d816c11240018660b8fa
Daulet Bimaganbetov <bimaganbetov1995@gmail.com>
2020-05-03 21:59:51 +0600

    contact page part 2

--- contact/models.py
+++ contact/models.py
@@ -1,1 +1,13 @@
 from django.db import models
+from django.urls import reverse
+
+class Contact(models.Model):
+	name = models.CharField(max_length=32)
+	email = models.EmailField()
+	text = models.TextField()
+
+	def __str__(self):
+		return self.email
+
+	def get_absolute_url(self):
+		return reverse('home')
--- contact/views.py
+++ contact/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import CreateView
+from .models import Contact
+from django.shortcuts import redirect
+from django.http import HttpResponse
 
-class ContactPageView(TemplateView):
+
+class ContactPageView(CreateView):
+	model = Contact
 	template_name='contact.html'
+	fields = ('name', 'email', 'text')
+
+	def form_valid(self, form):
+		return super().form_valid(form)
+
